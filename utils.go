e8622a5224506a419f9bf9241f5b2a152fd49c04
ToshihitoKon <toshihito.kon@gmail.com>
2024-06-25 13:47:48 +0900

    cleanup formatter and exporter

---
+++ utils.go
@@ -0,0 +1,24 @@
+package archive
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+)
+
+// NOTE: os.Getenv(ENVNAME) or os.Getenv(ENVNAME_BASE64)
+func getEnv(env string) string {
+	plain := os.Getenv(env)
+	b64 := os.Getenv(env + "_BASE64")
+	if plain != "" {
+		return plain
+	}
+	if b64 != "" {
+		decoded, err := base64.StdEncoding.DecodeString(b64)
+		if err != nil {
+			panic(fmt.Errorf("error: Environment variable decode: %s: %w", env+"_BASE64", err))
+		}
+		return string(decoded)
+	}
+	return ""
+}
