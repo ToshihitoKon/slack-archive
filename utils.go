631ed7d19c817c27c37b305db0027da73cb50e6e
ToshihitoKon <toshihito.kon@gmail.com>
2024-07-02 11:09:23 +0900

    便利なのでGetenvはarchive packageにして公開

--- utils.go
+++ utils.go
@@ -1,10 +1,35 @@
 package archive
 
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+)
+
 func firstString(slice []string) string {
 	for _, s := range slice {
 		if s != "" {
 			return s
 		}
+	}
+	return ""
+}
+
+// NOTE: os.Getenv(ENVNAME) or os.Getenv(ENVNAME_BASE64)
+func Getenv(env string) string {
+	envPrefix := "SA_"
+	plain := os.Getenv(envPrefix + env)
+	b64 := os.Getenv(envPrefix + env + "_BASE64")
+
+	if plain != "" {
+		return plain
+	}
+	if b64 != "" {
+		decoded, err := base64.StdEncoding.DecodeString(b64)
+		if err != nil {
+			panic(fmt.Errorf("error: Environment variable decode: %s: %w", env+"_BASE64", err))
+		}
+		return string(decoded)
 	}
 	return ""
 }
